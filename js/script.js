1cf0f42fcc1a3d4985effcb4e78a2e588ddb7eef
All <101561325+mritgur@users.noreply.github.com>
2022-10-03 12:26:02 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,52 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
-const nmi = document.querySelectorAll('.nav__menu_item'),
-        nav_submenu1 = document.querySelector('.nav_submenu_1'),
-        nav_submenu2 = document.querySelector('.nav_submenu_2'),
-        nav_submenu7 = document.querySelector('.nav_submenu_7'),
-        searchA = document.querySelector('.header__search_a'),
-        search = document.querySelector('.search');
- 
-
-
-    nmi[0].addEventListener('mouseenter', () => 
-        {
-          nav_submenu1.classList.add('nav_submenu_1_active');
-        });
-    nmi[0].addEventListener('mouseleave', () => 
-        {
-          nav_submenu1.classList.remove('nav_submenu_1_active');
-        });
-
-
-    nmi[1].addEventListener('mouseenter', () => 
-        {
-          nav_submenu2.classList.add('nav_submenu_2_active');
-        });
-    nmi[1].addEventListener('mouseleave', () => 
-        {
-          nav_submenu2.classList.remove('nav_submenu_2_active');
-        });
-
-
-
-    nmi[6].addEventListener('mouseenter', () => 
-        {
-          nav_submenu7.classList.add('nav_submenu_7_active');
-        });
-    nmi[6].addEventListener('mouseleave', () => 
-        {
-          nav_submenu7.classList.remove('nav_submenu_7_active');
-        });
-
-        searchA.addEventListener('click', (e) => {
-          e.preventDefault();
-          search.classList.add('search_active');
-        } );
-       
-   
-        window.addEventListener('click', (e)=>{
-          if (e.target == search) {
-            search.classList.remove('search_active');
-          }});
-
-    });
+  const nmi = document.querySelectorAll('.nav__menu_item'),
+          searchA = document.querySelector('.header__search_a'),
+          search = document.querySelector('.search');
+  
+          nmi.forEach((i)=>{
+            const submenu1 = i.querySelector('.nav_submenu_1');
+            const submenu2 = i.querySelector('.nav_submenu_2');
+            const submenu7 = i.querySelector('.nav_submenu_7');
+            if (submenu1) {
+              i.addEventListener('mouseenter',() =>
+              {
+                submenu1.classList.add('nav_submenu_1_active');
+              } );
+              i.addEventListener('mouseleave',() =>
+              {
+                submenu1.classList.remove('nav_submenu_1_active');
+              } );
+            }
+            else  if  (submenu2) {
+              i.addEventListener('mouseenter',() =>
+              {
+                submenu2.classList.add('nav_submenu_2_active');
+              } );
+              i.addEventListener('mouseleave',() =>
+              {
+                submenu2.classList.remove('nav_submenu_2_active');
+              } );
+            } 
+            else  if  (submenu7) {
+              i.addEventListener('mouseenter',() =>
+              {
+                submenu7.classList.add('nav_submenu_7_active');
+              } );
+              i.addEventListener('mouseleave',() =>
+              {
+                submenu7.classList.remove('nav_submenu_7_active');
+              } );
+            }
+          });
+          searchA.addEventListener('click', (e) => {
+            e.preventDefault();
+            search.classList.add('search_active');
+          } );
+          window.addEventListener('click', (e)=>{
+            if (e.target == search) {
+              search.classList.remove('search_active');
+            }});
+  
+      });
